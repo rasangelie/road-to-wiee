1a663cb004d66426c7b3fb8669e7a76bad4ee18b
Angelie <angeliebadar1@gmail.com>
2023-09-26 14:43:03 +0800

    added validation

--- scripts/00-try.js
+++ scripts/00-try.js
@@ -13,20 +13,29 @@ function display() {
     const timeFormat = getDateTime.toLocaleTimeString();
     
    if (getName === '') {
+        document.querySelector('.display')
+        .innerHTML='';
+
+        document.querySelector('input')
+            .classList.add('input-error');
+        
         document.querySelector('.js-error')
             .innerHTML= `Please enter your name to proceed`;
         
    } else {
+        
+
         document.querySelector('.js-error')
             .innerHTML='';
+
+        document.querySelector('input')
+            .classList.remove('input-error');
 
         document.querySelector('.display')
             .innerHTML= `Hello ${getName}! Today is ${dateFormat} and the time now is ${timeFormat}`;
         
         getInput.value = '';
+        
    }
-
-    
-
-    
+ 
 }
